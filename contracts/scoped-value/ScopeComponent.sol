f2f5b01eb96d5f2a6862da8e0345a9493f9fc65c
dk1a <dk1a@protonmail.com>
2023-02-15 20:20:06 +0300

    fix: use BYTES schema type for ScopeComponent

--- contracts/scoped-value/ScopeComponent.sol
+++ contracts/scoped-value/ScopeComponent.sol
@@ -2,8 +2,30 @@
 
 pragma solidity ^0.8.17;
 
-import { StringComponent } from "@latticexyz/std-contracts/src/components/StringComponent.sol";
+import { Component } from "solecs/Component.sol";
+import { LibTypes } from "solecs/LibTypes.sol";
 
-contract ScopeComponent is StringComponent {
-  constructor(address _world, uint256 _id) StringComponent(_world, _id) {}
+contract ScopeComponent is Component {
+  constructor(address world, uint256 id) Component(world, id) {}
+
+  function getSchema() public pure virtual override returns (string[] memory keys, LibTypes.SchemaValue[] memory values) {
+    keys = new string[](1);
+    values = new LibTypes.SchemaValue[](1);
+
+    keys[0] = "value";
+    values[0] = LibTypes.SchemaValue.BYTES;
+  }
+
+  function set(uint256 entity, string memory value) public virtual {
+    set(entity, abi.encode(value));
+  }
+
+  function getValue(uint256 entity) public view virtual returns (string memory) {
+    string memory value = abi.decode(getRawValue(entity), (string));
+    return value;
+  }
+
+  function getEntitiesWithValue(string memory value) public view virtual returns (uint256[] memory) {
+    return getEntitiesWithValue(abi.encode(value));
+  }
 }
